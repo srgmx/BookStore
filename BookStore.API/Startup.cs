781de0cd4d6bac3a4612a48e9c0a579c33d3b9b1
Serhii Maksymchuk <smakdealcase@gmail.com>
2020-11-11 17:58:44 +0200

    Added BookStoreDbContext and included to IoC container.

--- BookStore.API/Startup.cs
+++ BookStore.API/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Data.Persistance;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +15,19 @@ namespace BookStore.API
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookStoreDbContext>(o => 
+                o.UseSqlServer(_config.GetConnectionString("BookStoreDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ BookStore.Data/Persistance/BookStoreDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace BookStore.Data.Persistance
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+
+        public DbSet<Author> Author { get; set; }
+
+        public DbSet<Book> Book { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
