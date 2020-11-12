3353ba08d4a151bcb5059e5308ecd85357572e97
Serhii Maksymchuk <smakdealcase@gmail.com>
2020-11-12 19:00:20 +0200

    Added user service.

---
+++ BookStore.Service/Contracts/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookStore.Business.Dto;
+
+namespace BookStore.Business.Contracts
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDto>> GetUsersAsync();
+
+        Task<UserDto> GetUserByIdAsync(int id);
+        Task<UserDto> AddUserAsync(UserDto user);
+
+        Task<UserDto> UpdateUserAsync(UserDto user);
+
+        Task<UserDto> RemoveUserByIdAsync(int id);
+    }
+}
---
+++ BookStore.Service/Services/UserService.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookStore.Domain;
+using BookStore.Data.Contracts;
+using BookStore.Business.Contracts;
+using BookStore.Business.Dto;
+
+
+namespace BookStore.Business.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IGenericRepository<User> _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserService(IGenericRepository<User> userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDto> AddUserAsync(UserDto user)
+        {
+            var userToAdd = _mapper.Map<UserDto, User>(user);
+            var userInDb = await _userRepository.AddAsync(userToAdd);
+            user.Id = userInDb.Id;
+
+            return user;
+        }
+
+        public async Task<UserDto> GetUserByIdAsync(int id)
+        {
+            var userInDb = await _userRepository.FindByIdAsync(id);
+
+            return _mapper.Map<User, UserDto>(userInDb);
+        }
+
+        public async Task<IEnumerable<UserDto>> GetUsersAsync()
+        {
+            var users = await _userRepository.FindAllAsync();
+
+            return _mapper.Map<IEnumerable<User>, IEnumerable<UserDto>>(users);
+        }
+
+        public async Task<UserDto> RemoveUserByIdAsync(int id)
+        {
+            var userInDb = await _userRepository.FindByIdAsync(id);
+
+            if (userInDb == null) return null;
+
+            var userRemoved = await _userRepository.RemoveAsync(userInDb);
+
+            return _mapper.Map<User, UserDto>(userRemoved);
+        }
+
+        public async Task<UserDto> UpdateUserAsync(UserDto user)
+        {
+            var userInDb = await _userRepository.FindByIdAsync(user.Id);
+
+            if (userInDb == null) return null;
+
+            var userToUpdate = _mapper.Map<UserDto, User>(user);
+            await _userRepository.UpdateAsync(userToUpdate);
+            
+            return user; 
+        }
+    }
+}
