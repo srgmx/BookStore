583377efe8b5a8eede09ae17a24e0d4789722834
Serhii Maksymchuk <smakdealcase@gmail.com>
2020-11-12 18:57:23 +0200

    Added Authomaper packages and profile, UserDto.

---
+++ BookStore.Service/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BookStore.Business.Dto;
+using BookStore.Domain;
+
+namespace BookStore.Business.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
