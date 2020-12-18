77a7ae9ee36afdca00307524479daa400bc7ddd5
Serhii Maksymchuk <smakdealcase@gmail.com>
2020-12-19 01:22:13 +0200

    Refactored MappingProfile

--- BookStore.Business/Mapping/MappingProfile.cs
+++ BookStore.Business/Mapping/MappingProfile.cs
@@ -8,57 +8,45 @@ namespace BookStore.Business.Mapping
     {
         public MappingProfile()
         {
+            SetUserMapping();
+            SetAuthorMapping();
+            SetBookMapping();
+        }
+
+        private void SetUserMapping()
+        {
             CreateMap<User, UserDto>();
+
             CreateMap<UserDto, User>();
+        }
 
+        private void SetAuthorMapping()
+        {
             CreateMap<Book, AuthorBookDto>()
-                .ForMember(
-                    d => d.BookId,
-                    o => o.MapFrom(s => s.Id)
-                )
-                .ForMember(
-                    d => d.BookName,
-                    o => o.MapFrom(s => s.Name)
-                );
+                .ForMember(d => d.BookId, o => o.MapFrom(s => s.Id))
+                .ForMember(d => d.BookName, o => o.MapFrom(s => s.Name));
 
-            CreateMap<Author, BookAuthorDto>()
-                .ForMember(
-                    d => d.FirstName,
-                    o => o.MapFrom(s => s.User.FirstName)
-                )
-                .ForMember(
-                    d => d.LastName,
-                    o => o.MapFrom(s => s.User.LastName)
-                )
-                .ForMember(
-                    d => d.UserId,
-                    o => o.MapFrom(s => s.User.Id)
-                );
             CreateMap<Author, AuthorDto>()
-                .ForMember(
-                    d => d.FirstName,
-                    o => o.MapFrom(s => s.User.FirstName)
-                )
-                .ForMember(
-                    d => d.LastName,
-                    o => o.MapFrom(s => s.User.LastName)
-                )
-                .ForMember(
-                    d => d.UserId,
-                    o => o.MapFrom(s => s.User.Id)
-                )
-                .ForMember(
-                    d => d.AuthorBooks,
-                    o => o.MapFrom(s => s.Books)
-                );
+                .ForMember(d => d.FirstName, o => o.MapFrom(s => s.User.FirstName))
+                .ForMember(d => d.LastName, o => o.MapFrom(s => s.User.LastName))
+                .ForMember(d => d.UserId, o => o.MapFrom(s => s.User.Id))
+                .ForMember(d => d.AuthorBooks, o => o.MapFrom(s => s.Books));
+
             CreateMap<AuthorToAddDto, Author>();
+
             CreateMap<AuthorToUpdateDto, Author>();
+        }
+
+        private void SetBookMapping()
+        {
+            CreateMap<Author, BookAuthorDto>()
+                .ForMember(d => d.FirstName, o => o.MapFrom(s => s.User.FirstName))
+                .ForMember(d => d.LastName, o => o.MapFrom(s => s.User.LastName))
+                .ForMember(d => d.UserId, o => o.MapFrom(s => s.User.Id));
 
             CreateMap<BookToAddDto, Book>()
-                .ForMember(
-                    d => d.Authors, 
-                    o => o.Ignore()
-                 );
+                .ForMember(d => d.Authors, o => o.Ignore());
+
             CreateMap<Book, BookDto>();
         }
     }
